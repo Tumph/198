3eb0408a5f16620fae0ca111095688b87ef3de8d
Aryan Gupta <64933084+Tumph@users.noreply.github.com>
2023-11-15 01:32:18 -0500

    Update main.c
    commit 3

--- main.c
+++ main.c
@@ -18,7 +18,9 @@ static void MX_GPIO_Init(void);
 
 static void MX_ADC1_Init(void);
 
-void Error_Handler(void);
+
+
+float calculateUVIndex(uint16_t uvData);
 
 
 
@@ -26,53 +28,117 @@ int main(void)
 
 {
 
-    HAL_Init();
+  HAL_Init();
 
-    SystemClock_Config();
-
-
-
-    MX_GPIO_Init();
-
-    MX_ADC1_Init();
+  SystemClock_Config();
 
 
 
-    uint32_t uv_data;
+  MX_GPIO_Init();
+
+  MX_ADC1_Init();
 
 
 
-    while (1)
+  // Variables for UV data and timing
+
+  uint16_t uvData = 0;
+
+  uint32_t startTime = 0;
+
+  uint32_t endTime = 0;
+
+
+
+  while (1)
+
+  {
+
+    // Check if the black button (B2) is pressed to start UV exposure tracking
+
+    if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10) == GPIO_PIN_SET)
 
     {
 
-        // Check if the black button (B2) is pressed to start recording UV data
+      // Start UV exposure tracking
 
-        if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET) // Assuming B2 is connected to PC13
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET); // Set GPIOA Pin 5 high
+
+
+
+      // Record the start time
+
+      startTime = HAL_GetTick();
+
+      printf("UV Exposure Tracking Started!\r\n");
+
+
+
+      // Read UV data from the sensor
+
+      if (HAL_ADC_Start(&hadc1) == HAL_OK &&
+
+          HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY) == HAL_OK)
+
+      {
+
+        uvData = HAL_ADC_GetValue(&hadc1);
+
+      }
+
+
+
+      // Display UV data in the terminal
+
+      printf("UV Data: %d\r\n", uvData);
+
+
+
+      // Delay for a short period (adjust as needed)
+
+      HAL_Delay(1000);
+
+
+
+      // Stop UV exposure tracking if the button is pressed again
+
+      if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10) == GPIO_PIN_SET)
+
+      {
+
+        // Record the end time
+
+        endTime = HAL_GetTick();
+
+        printf("UV Exposure Tracking Stopped!\r\n");
+
+
+
+        // Stop UV exposure tracking
+
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET); // Reset GPIOA Pin 5 low
+
+
+
+        // Calculate and display the final UV index
+
+        float uvIndex = calculateUVIndex(uvData);
+
+
+
+        // Add your post-exposure logic here
+
+        if (uvIndex > 6.0)
 
         {
 
-            // Start recording UV data
+          // UV index is high, prompt the user to take protective measures
 
-            HAL_ADC_Start(&hadc1);
+          printf("UV index is high. Take necessary precautions!\r\n");
 
-            HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+          // Add your code to trigger warnings, alerts, or other actions
 
-            uv_data = HAL_ADC_GetValue(&hadc1);
-
-            HAL_ADC_Stop(&hadc1);
-
-
-
-            // Output UV data to the terminal
-
-            printf("UV Data: %lu\r\n", uv_data);
-
-
-
-            // Wait for a short duration before taking the next reading
-
-            HAL_Delay(1000);
+          // For example, flashing LEDs, sending notifications, etc.
 
         }
 
@@ -80,25 +146,57 @@ int main(void)
 
         {
 
-            // Stop recording when the black button (B2) is pressed again
+          // UV index is within the safe range
 
-            HAL_ADC_Stop(&hadc1);
+          printf("UV index is within the safe range.\r\n");
 
-
-
-            // Wait for the button to be released
-
-            while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_SET)
-
-            {
-
-                // Add any other code or functionality needed during the idle state
-
-            }
+          // Add your code for normal operation or display a message
 
         }
 
+
+
+        // Your additional logic goes here
+
+
+
+        // Delay for a reasonable period before repeating the loop
+
+        HAL_Delay(5000); // Adjust the delay time as needed
+
+      }
+
     }
+
+  }
+
+}
+
+
+
+// Function to calculate and display the final UV index
+
+float calculateUVIndex(uint16_t uvData)
+
+{
+
+  // Implement your UV index calculation logic based on sensor data
+
+  // For example, a simple linear mapping from UV data to UV index
+
+  // This is a placeholder and should be replaced with your actual calculation
+
+  float uvIndex = (float)uvData / 100.0;
+
+
+
+  // Display the calculated UV index
+
+  printf("Final UV Index: %.2f\r\n", uvIndex);
+
+
+
+  return uvIndex;
 
 }
 
@@ -108,59 +206,59 @@ void SystemClock_Config(void)
 
 {
 
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
 
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-
-
-    __HAL_RCC_PWR_CLK_ENABLE();
-
-    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
 
 
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  __HAL_RCC_PWR_CLK_ENABLE();
 
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-
-    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
 
 
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
 
-    {
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
 
-        Error_Handler();
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
 
-    }
-
-
-
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
-
-                                  RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
 
 
 
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
 
-    {
+  {
 
-        Error_Handler();
+    Error_Handler();
 
-    }
+  }
+
+
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+
+                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+
+  {
+
+    Error_Handler();
+
+  }
 
 }
 
@@ -170,59 +268,59 @@ static void MX_ADC1_Init(void)
 
 {
 
-    hadc1.Instance = ADC1;
+  hadc1.Instance = ADC1;
 
-    hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
 
-    hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
 
-    hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ScanConvMode = DISABLE;
 
-    hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
 
-    hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
 
-    hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
 
-    hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
 
-    hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
 
-    hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.NbrOfConversion = 1;
 
-    hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
 
-    hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-
-
-
-    if (HAL_ADC_Init(&hadc1) != HAL_OK)
-
-    {
-
-        Error_Handler();
-
-    }
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
 
 
 
-    ADC_ChannelConfTypeDef sConfig = {0};
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
 
-    sConfig.Channel = ADC_CHANNEL_0;
+  {
 
-    sConfig.Rank = 1;
+    Error_Handler();
 
-    sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  }
 
 
 
-    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  ADC_ChannelConfTypeDef sConfig = {0};
 
-    {
+  sConfig.Channel = ADC_CHANNEL_0;
 
-        Error_Handler();
+  sConfig.Rank = 1;
 
-    }
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+
+
+
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+
+  {
+
+    Error_Handler();
+
+  }
 
 }
 
@@ -232,25 +330,47 @@ static void MX_GPIO_Init(void)
 
 {
 
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
 
 
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-
-    __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
 
 
-    // Configure GPIO pin PC13 as input with pull-up
+  // Initialize the UV sensor pin (Update with the appropriate pin for your UV sensor)
 
-    GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Pin = GPIO_PIN_0; // Update with the appropriate pin for your UV sensor (e.g., GPIO_PIN_A0)
 
-    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
 
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
 
-    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+
+
+  // Initialize the button pin
+
+  GPIO_InitStruct.Pin = GPIO_PIN_10;
+
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+
+
+  // Initialize the UV exposure tracking indicator pin
+
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
 
@@ -260,29 +380,19 @@ void Error_Handler(void)
 
 {
 
-    // Add error handling code as needed
+  while (1)
 
-    while (1)
+  {
 
-    {
+    // An error occurred, stay in this loop
 
-        // Infinite loop to indicate an error
-
-    }
+  }
 
 }
 
 
 
-#ifdef USE_FULL_ASSERT
+#ifdef USE_FULL
 
-void assert_failed(uint8_t *file, uint32_t line)
-
-{
-
-    // User can add his own implementation to report the file name and line number
-
-}
-
-#endif /* USE_FULL_ASSERT */
+#endif // Add this
 
